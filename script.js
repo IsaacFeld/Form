e2b1920068d8768e67786c0fb2b4f6383cfa4be8
IsaacFeld <isaacbfeldman@gmail.com>
2024-10-30 09:09:25 +0100

    Added interaction for buttons

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const submitButton = document.querySelector('.btn-submit');
 const streamCount = document.getElementById('streamCount')
 const steps = document.querySelectorAll('.step');
 const form_steps = document.querySelectorAll('.form-step');
-console.log(steps.length);
+const forgot = document.getElementById('forgot');
+maxStreams = 0;
+maxStreamsInt = 0;
 let active = 4;
 
 streamCount.addEventListener('click', () => {
-  console.log("hello");
+  
 })
 nextButton.addEventListener('click', () => {
   active++;
@@ -28,10 +30,40 @@ prevButton.addEventListener('click', () => {
 submitButton.addEventListener('click', () =>{
   print();
 })
-
+function isNumber(value){
+  return typeof value === 'number';
+  
+}
+function checkSubmit(){
+  console.log(submitButton.disabled);
+  if(submitButton.disabled == true){
+    nextButton.style.display = "inline-block";
+    submitButton.style.display = "none";
+  }
+  else{
+    nextButton.style.display = "none";
+    submitButton.style.display = "inline-block";
+  }
+}
 const updateProgress = () => {
-  console.log('steps.length =>' + steps.length);
-  console.log('active =>' + active);
+  forgot.style.display = "none";
+  maxStreamsInt = parseInt(streamCount.value)
+  if(isNaN(maxStreamsInt)){
+    maxStreamsInt = 0;
+  }
+  else{
+    
+  }
+  
+
+  
+ // console.log('steps.length =>' + steps.length);
+  //console.log('active => ' + active);
+  if(active == 6){
+      maxStreams = 6 + maxStreamsInt;
+    
+
+  }
   for (let i = 0; i < active; i++){
     steps[i].classList.add('behindactive');
     form_steps[i].classList.add('behindactive')
@@ -47,30 +79,37 @@ const updateProgress = () => {
     if (i == (active - 1)) {
       steps.classList.add('active');
       form_steps[i].classList.add('active');
-      console.log('i =>' + i);
+      //console.log('i =>' + i);
     } else {
       steps.classList.remove('active');
      
       form_steps[i].classList.remove('active');
       
-      console.log('i =>' + i);
+     // console.log('i =>' + i);
     }
   });
-  console.log('active for buttons' + active);
   //enable or disable prev and next buttons
   if (active === 1) {
     prevButton.disabled = true;
     nextButton.disabled = false;
+    submitButton.disabled = true;
+    
   }
 
-  if (active === steps.length) {
+  if (active === maxStreams) {
+    if(active == 6){
+      forgot.style.display = "inline-block";
+    }
+    
     nextButton.disabled = true;
     prevButton.disabled = false;
+    submitButton.disabled = false;
   }
   else {
-    console.log("all buttons are enabled")
+    submitButton.disabled = true;
     prevButton.disabled = false;
     nextButton.disabled = false;
   }
+  checkSubmit();
 }
 updateProgress();
