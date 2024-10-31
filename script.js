2598a994a584e88799641487e4aade3326ad7e31
Pugcy <isaacbfeldman@gmail.com>
2024-10-31 09:32:50 +0100

    Updated script.js to Final Version

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const steps = document.querySelectorAll('.step');
 const form_steps = document.querySelectorAll('.form-step');
 const forgot = document.getElementById('forgot');
 const stepbtns = document.querySelectorAll('.btn-steps');
+const logos = document.querySelectorAll('#formgraphics');
+const progress = document.getElementsByClassName('progress');
 maxStreams = 0;
 maxStreamsInt = 0;
-let active = 4;
+let active = 1;
 for(let i = 0; i < 7; i++){
   stepbtns[i].addEventListener('click', () => {
     updateProgress();
     if(i == 6 && noAccessToStream == true){
-      console.log("hello i got here");
     }
     else{
       active = i+1;
@@ -22,10 +23,8 @@ for(let i = 0; i < 7; i++){
     
   })
 }
-
-streamCount.addEventListener('click', () => {
-  
-})
+//Check for whether the user has selected a number of streams or not, 
+//if they have not then we do not let the user progress to the next step
 nextButton.addEventListener('click', () => {
   active++;
   if (active > steps.length) {
@@ -33,7 +32,6 @@ nextButton.addEventListener('click', () => {
   }
   updateProgress();
 })
-
 prevButton.addEventListener('click', () => {
   active--;
   if (active < 1) {
@@ -41,13 +39,20 @@ prevButton.addEventListener('click', () => {
   }
   updateProgress();
 })
-submitButton.addEventListener('click', () =>{
-  print();
-})
-function isNumber(value){
-  return typeof value === 'number';
+//Setting up for nice print without clutter
+submitButton.addEventListener('click', () => {
+  submitButton.disabled = true;
+  checkSubmit();
+  nextButton.style.display = "none";
+  progress[0].style.display = "none";
   
-}
+  for(let i = 0; i < maxStreams; i++){
+      logos[i].style.display = "none";
+      steps[i].classList.add('active');
+      form_steps[i].classList.add('active');
+  }
+});
+
 function checkSubmit(){
   if(submitButton.disabled == true){
     nextButton.style.display = "inline-block";
@@ -59,28 +64,33 @@ function checkSubmit(){
   }
 }
 const updateProgress = () => {
+  progress[0].style.display = "inline-block";
+  for(let i = 0; i < maxStreams; i++){
+    logos[i].style.display = "inline-block";
+  }
   forgot.style.display = "none";
   maxStreamsInt = parseInt(streamCount.value)
-  if(isNaN(maxStreamsInt)){
+  if(isNaN(maxStreamsInt) || maxStreamsInt == 0 ){
     console.log("noaccess");
     maxStreamsInt = 0;
     noAccessToStream = true;
   }
+
   else{
     noAccessToStream = false;
   }
-  
-
-  
- // console.log('steps.length =>' + steps.length);
-  //console.log('active => ' + active);
+  //On page 6 we check if they selected streams,
+  //Max Stream pages shown calculated here
   if(active == 6){
       maxStreams = 6 + maxStreamsInt;
   }
+  //Same thing, in case they skip straight to 7
+  //so we know how many to display
   if(active == 7){
     maxStreams = 6 + maxStreamsInt;
   }
-  for (let i = 0; i < active; i++){
+  //Color in circles behind current one to simulate progress
+  for (let i = 0; i < active -1; i++){
     steps[i].classList.add('behindactive');
     form_steps[i].classList.add('behindactive')
   }
@@ -88,30 +98,24 @@ const updateProgress = () => {
     steps[i].classList.remove('behindactive');
     form_steps[i].classList.remove('behindactive')
   }
-
-  //toggle active class for each list item
+  //Handling active page, what step to show:
   steps.forEach((steps, i) => {
-
     if (i == (active - 1)) {
       steps.classList.add('active');
       form_steps[i].classList.add('active');
-      //console.log('i =>' + i);
     } else {
       steps.classList.remove('active');
-     
       form_steps[i].classList.remove('active');
-      
-     // console.log('i =>' + i);
     }
   });
   //enable or disable prev and next buttons
+  // and forgot stream message
   if (active === 1) {
     prevButton.disabled = true;
     nextButton.disabled = false;
     submitButton.disabled = true;
     
   }
-
   if (active === maxStreams) {
     if(active == 6){
       forgot.style.display = "inline-block";
@@ -129,3 +133,4 @@ const updateProgress = () => {
   checkSubmit();
 }
 updateProgress();
+
